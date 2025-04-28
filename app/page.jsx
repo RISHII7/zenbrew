02298b4ee271ab3db67d899a1a85348c63648d1d
RISHII7 <rishikesh.palande07@gmail.com>
2025-04-28 23:42:51 +0530

    Added Hero Section with Badge and Separator

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,16 @@
 "use client"
 
 import Hero from '@/components/Hero'
+import { useEffect } from 'react'
 
 const Home = () => {
   return (
     <div className='h-full overflow-x-hidden'>
       <Hero />
+      {/* Temporary Div */}
+      <div className='h-[4000px]'>
+
+      </div>
     </div>
   )
 }
