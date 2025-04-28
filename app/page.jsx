1c14951648e9dcad637a083e664f0d02626bf14f
RISHII7 <rishikesh.palande07@gmail.com>
2025-04-28 23:48:40 +0530

    Added Locomotive Scroll to the website

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,15 @@ import Hero from '@/components/Hero'
 import { useEffect } from 'react'
 
 const Home = () => {
+  // Add Locomotive Scroll
+  useEffect(() => {
+    const loadLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      new LocomotiveScroll();
+    };
+
+    loadLocomotiveScroll();
+  }, []);
   return (
     <div className='h-full overflow-x-hidden'>
       <Hero />
